4cc38be7679bf508cc3e17c788df608e3754e3f2
yyokii <yyokii.h@gmail.com>
2022-05-13 22:10:22 +0900

    🩹

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import Layout from '../components/Layout'
 
 export default function Home() {
   return (
+    // TODO: サインイン、会員登録機能への導線を追加する
     <Layout>
       <div className="text-center">
-        <div className="row">
+        <div className="row justify-content-center">
           <div className="col-12 col-md-6">
             <h1>Myサービス</h1>
             <p>ここはサービスです。</p>
