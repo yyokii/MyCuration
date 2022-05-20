14f9330b6faf7815826a0c831ba3a9613bde327c
yyokii <yyokii.h@gmail.com>
2022-05-21 02:04:37 +0900

    🚧 WIP: auth redirect

---
+++ src/pages/onboarding.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+export default function Onboarding() {
+  return (
+    <Layout>
+      <div className='text-center'>
+        <div className='row justify-content-center'>
+          <div className='row col-12 col-md-6'>
+            <h1>ユーザー情報入力画面</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Layout from '../components/Layout'
 import { login, logout } from '../lib/firebase-auth'
 
@@ -17,8 +18,13 @@ export default function Signup() {
         <div className='row justify-content-center'>
           <div className='col-12 col-md-6'>
             <h1>サインイン</h1>
+            <Link href='/auth-redirect?redirect_uri=onboarding'>
+              <a className='mt-5 btn btn-primary' role='button'>
+                ログイン
+              </a>
+            </Link>
             <button onClick={handleLogin}>ログイン</button>
-            <button onClick={handleLogout}>ログアウト</button>
+            {/* <button onClick={handleLogout}>ログアウト</button> */}
           </div>
         </div>
       </div>
