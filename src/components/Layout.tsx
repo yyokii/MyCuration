b58d58d5ed5d82ed9857734ec21babab58f44a28
yyokii <yyokii.h@gmail.com>
2022-05-29 02:47:05 +0900

    ✨ Add header and footer template

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ToastContainer } from 'react-toastify'
-import Link from 'next/link'
 import Head from 'next/head'
+import Footer from './Fotter'
+import Header from './Header'
 
 export default function Layout({ children }) {
   const title = 'Myサービス'
@@ -19,45 +20,10 @@ export default function Layout({ children }) {
           rel='stylesheet'
         ></link>
       </Head>
-      <nav
-        className='navbar navbar-expand-lg navbar-light mb-3'
-        style={{ backgroundColor: '#e3f2fd' }}
-      >
-        <div className='container'>
-          <div className='mr-auto'>
-            <Link className='navbar-brand' href='/'>
-              Myサービス
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <Header />
       <div className='container'>{children}</div>
       <ToastContainer />
-      <nav className='navbar fixed-bottom navbar-light bg-light'>
-        <div className='container'>
-          <div className='d-flex justify-content-between align-items-center w-100'>
-            <i className='material-icons'>menu</i>
-            <Link href='/questions/received'>
-              <a>
-                <i className='material-icons'>home</i>
-              </a>
-            </Link>
-            <Link href='/users/me'>
-              <a>
-                <i className='material-icons'>person</i>
-              </a>
-            </Link>
-          </div>
-        </div>
-      </nav>
-      <footer className='text-center mt-5 py-5 bg-light'>
-        <div className='pb-1 text-muted'>
-          Created by{' '}
-          <a href='https://twitter.com/enyyokii' className='link-info'>
-            @enyyokii
-          </a>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
