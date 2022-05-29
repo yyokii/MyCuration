cc16ed240441107556b14e9b961c7948dfd7c903
yyokii <yyokii.h@gmail.com>
2022-05-29 21:14:00 +0900

    🎨 Fix signin page ui

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, StackDivider, VStack } from '@chakra-ui/react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { login, logout } from '../lib/firebase-auth'
@@ -9,19 +10,13 @@ export default function Signup() {
 
   return (
     <Layout>
-      <div className='text-center'>
-        <div className='row justify-content-center'>
-          <div className='col-12 col-md-6'>
-            <h1>サインイン</h1>
-            <Link href='/auth-redirect?redirect_uri=onboarding'>
-              <a className='mt-5 btn btn-primary' role='button'>
-                ログイン
-              </a>
-            </Link>
-            {/* <button onClick={handleLogout}>ログアウト</button> */}
-          </div>
-        </div>
-      </div>
+      <VStack divider={<StackDivider borderColor='gray.200' />} spacing={4} align='center'>
+        <Box h='40px' my={5}>
+          <Link href='/auth-redirect?redirect_uri=onboarding' passHref>
+            <Button as='a'>ログイン</Button>
+          </Link>
+        </Box>
+      </VStack>
     </Layout>
   )
 }
