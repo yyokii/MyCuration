54bf35d32522ac4b7395e0b99acdbd37ebff73b0
yyokii <yyokii.h@gmail.com>
2022-06-12 23:35:15 +0900

    🎨 Update article item UI

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    // TODO: remove this when we dont need it anymore
+    domains: ['lh3.googleusercontent.com', `images.unsplash.com`],
   },
 }
 
