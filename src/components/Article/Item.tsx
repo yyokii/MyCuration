ea40c93104d78ddddf2615684a68aca9009742c2
yyokii <yyokii.h@gmail.com>
2022-06-14 23:49:32 +0900

    ⚰️  Remove not using line

--- src/components/Article/Item.tsx
+++ src/components/Article/Item.tsx
@@ -94,28 +94,5 @@ export default function Item(props: Props) {
         </Stack>
       </Box>
     </Center>
-    // <div key={props.article.id}>
-    //   <div className='card my-3'>
-    //     {props.isCurrentUser && (
-    //       <div>
-    //         <div className='m-1 text-end' onClick={() => props.onClickDelete(props.article)}>
-    //           <i className='material-icons'>delete</i>
-    //         </div>
-    //       </div>
-    //     )}
-    //     <div>
-    //       <VStack>
-    //         <div className='text-truncate'>{props.article.contentURL}</div>
-    //         <HStack>
-    //           {props.article.displayTags.length > 0 &&
-    //             props.article.displayTags.map((tag) => <Box key={tag}>{tag}</Box>)}
-    //         </HStack>
-    //       </VStack>
-    //       <div className='text-muted text-end'>
-    //         <small>{dayjs(props.article.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}</small>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
