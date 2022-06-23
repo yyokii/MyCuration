d547e04f016c64c3ade3ff7392b77d0e599fe71e
yyokii <yyokii.h@gmail.com>
2022-06-24 00:39:23 +0900

    ♻️  Add create article api

---
+++ src/pages/api/article.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import {
+  sendInternalServerErrorResponse,
+  sendNotImplementedErrorResponse,
+} from '../../utils/api/sendErrorResponse'
+import { createArticle } from '../../lib/db-admin'
+import { authenticate } from '../../utils/api/authenticate'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    try {
+      const body = JSON.parse(req.body)
+      console.log('body', body)
+      const article = await createArticle(
+        req.query.uid as string,
+        body.contentURL,
+        body.comment,
+        body.tags,
+      )
+      return res.status(200).json(article)
+    } catch (error) {
+      console.log(error)
+      return sendInternalServerErrorResponse(res, error.message)
+    }
+  } else {
+    sendNotImplementedErrorResponse(res, 'This method is not allowed.')
+  }
+}
+
+export default authenticate(handler)
