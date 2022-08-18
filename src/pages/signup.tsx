35603f9cb65789955bb753fdee0816c7d63ed7fa
yyokii <yyokii.h@gmail.com>
2022-08-19 01:42:56 +0900

    🩹 Fix sign up button

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,5 @@
-import { Button, StackDivider, VStack } from '@chakra-ui/react'
+import { Box, Button, StackDivider, VStack } from '@chakra-ui/react'
+import Link from 'next/link'
 import { FaGoogle } from 'react-icons/fa'
 import Layout from '../components/Layout'
 
@@ -6,9 +7,13 @@ export default function Signup() {
   return (
     <Layout>
       <VStack divider={<StackDivider borderColor='gray.200' />} spacing={4} align='center'>
-        <Button my={8} colorScheme='teal' leftIcon={<FaGoogle />}>
-          Sign in with Google
-        </Button>{' '}
+        <Box h='40px' my={8}>
+          <Link href='/auth-redirect?redirect_uri=onboarding' passHref>
+            <Button as='a' colorScheme='teal' leftIcon={<FaGoogle />}>
+              Sign in with Google
+            </Button>
+          </Link>
+        </Box>
       </VStack>
     </Layout>
   )
