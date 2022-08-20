7feb6f94522d53c4b2f2b4aba2fddb537fde6aa0
yyokii <yyokii.h@gmail.com>
2022-08-20 23:11:10 +0900

    🎨 Fix article item UI

--- src/components/Article/Item.tsx
+++ src/components/Article/Item.tsx
@@ -9,6 +9,7 @@ import {
   useColorModeValue,
   Circle,
   Flex,
+  VStack,
 } from '@chakra-ui/react'
 import dayjs from 'dayjs'
 import { Article } from '../../types/Article'
@@ -22,18 +23,20 @@ type Props = {
 }
 
 export default function Item(props: Props) {
+  const basePadding = 6
   return (
-    <Center py={6}>
+    <Center py={basePadding}>
       <Box
         maxW={'400px'}
         w={'250px'}
         bg={useColorModeValue('white', 'gray.900')}
         boxShadow={'2xl'}
         rounded={'md'}
-        p={6}
+        p={basePadding}
         overflow={'hidden'}
       >
-        <Box h={'210px'} bg={'gray.100'} mt={-6} mx={-6} mb={6} pos={'relative'}>
+        <Box h={'120px'} bg={'gray.100'} mt={-basePadding} mx={-basePadding} pos={'relative'}>
+          {/*  */}
           <Image
             src={
               'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
@@ -42,9 +45,9 @@ export default function Item(props: Props) {
             alt={'Article OGP'}
           />
         </Box>
-        <Stack>
+        <Stack spacing={2}>
           {props.isCurrentUser && (
-            <Flex justifyContent='end' alignContent='center'>
+            <Flex justifyContent='end' alignContent='center' mt={4}>
               <Circle
                 mr={2}
                 size='24px'
@@ -64,9 +67,9 @@ export default function Item(props: Props) {
               </Circle>
             </Flex>
           )}
-          <HStack>
-            {props.article.displayTags.length > 0 &&
-              props.article.displayTags.map((tag) => (
+          {props.article.displayTags.length > 0 && (
+            <HStack>
+              {props.article.displayTags.map((tag) => (
                 <Text
                   color={'green.500'}
                   fontWeight={800}
@@ -77,19 +80,23 @@ export default function Item(props: Props) {
                   {tag}
                 </Text>
               ))}
-          </HStack>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}
-          >
-            {props.article.contentURL}
-          </Heading>
-          <Text color={'gray.500'}>{props.article.comment}</Text>
-        </Stack>
-        <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-          <Text color={'gray.500'}>
-            {dayjs(props.article.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}
+            </HStack>
+          )}
+          <VStack align={'start'} spacing={0}>
+            <Text color={'gray.500'}>
+              {dayjs(props.article.createdAt.toDate()).format('YYYY/MM/DD HH:mm')}
+            </Text>
+            <Heading
+              color={useColorModeValue('gray.700', 'white')}
+              fontSize={'2xl'}
+              fontFamily={'body'}
+              pt={0}
+            >
+              {props.article.contentURL}
+            </Heading>
+          </VStack>
+          <Text color={'gray.500'} noOfLines={2}>
+            {props.article.comment}
           </Text>
         </Stack>
       </Box>
