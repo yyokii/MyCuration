690db053bda6d2a0f6dc66fe2ffdaae2af8f0ccc
yyokii <yyokii.h@gmail.com>
2022-09-19 12:02:46 +0900

    🩹 Add sign out and delete account button in user page.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,35 +2,13 @@ import { ToastContainer } from 'react-toastify'
 import Head from 'next/head'
 import Footer from './Fotter'
 import Header from './Header'
-import { signOut } from '../lib/firebase-auth'
-import { RepositoryFactory } from '../repository/repository'
-import { UserRepository } from '../repository/userRepository'
-import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
   const title = 'Myサービス'
   const description = '〜なサービスです。'
   const ogpImageUrl = `${process.env.NEXT_PUBLIC_WEB_URL}/images/ogp_card.png`
 
-  const router = useRouter()
-
-  const userRepository: UserRepository = RepositoryFactory.get('user')
-
-  const menuContents = [
-    {
-      title: 'Sign out',
-      action: async () => {
-        await signOut()
-      },
-    },
-    {
-      title: 'Delete account',
-      action: async () => {
-        await userRepository.delete()
-        router.push('/')
-      },
-    },
-  ]
+  const menuContents = []
 
   return (
     <div>
