e772065711a16a509586f9e88e16db65af07f41a
yyokii <yyokii.h@gmail.com>
2022-12-13 00:25:19 +0900

    🎨 Fix header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,8 +24,8 @@ export default function Header(props: Props) {
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+      <Box bg={useColorModeValue('red.200', 'gray.900')} px={4}>
+        <Flex h={8} alignItems={'center'} justifyContent={'space-between'}>
           <HStack spacing={8} alignItems={'center'}>
             <Box>Logo</Box>
           </HStack>
