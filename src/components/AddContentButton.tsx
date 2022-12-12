7e87ec69e571adc89d78e7c0e83a76c3a405dbb5
yyokii <yyokii.h@gmail.com>
2022-12-13 00:21:32 +0900

    🎨 Fix add button ui

---
+++ src/components/AddContentButton.tsx
@@ -0,0 +1,24 @@
+import { Button, ButtonProps } from '@chakra-ui/react'
+
+export default function AddContentButton(props: ButtonProps) {
+  return (
+    <Button
+      {...props}
+      px={4}
+      fontSize={'sm'}
+      rounded={'full'}
+      color={'red.400'}
+      boxShadow='2xl'
+      p='6'
+      bg='white'
+      _hover={{
+        bg: 'gray.100',
+      }}
+      _focus={{
+        bg: 'gray.100',
+      }}
+    >
+      Add Content
+    </Button>
+  )
+}
