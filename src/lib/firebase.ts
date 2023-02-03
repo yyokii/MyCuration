3cbda916e0ee897e91193a70336e3d4ae64a7654
yyokii <yyokii.h@gmail.com>
2023-02-04 00:54:57 +0900

    🩹

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -19,7 +19,7 @@ if (getApps().length === 0) {
   // 初期化済みでない場合に実行
   const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    authDomain: 'my-curation.vercel.app',
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
